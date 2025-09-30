313c0d7a8bc59f5e1b8ceb9b7440302c7836b7b3
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-29 20:40:02 -0700

    exercise 04, sitll working on...

--- ex04.js
+++ ex04.js
@@ -1,5 +1,42 @@
 // Exercise 4: Write a function `mostFrequentChar` that takes a string and returns the character
-// that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
+// that appears the most times. If there is a tie, 
+// return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
+// .map? .reduce?
+// split the str
+// for i <= splitChar.length
+// if char, if it's existed +1
+
+const mostFrequentChar = function (str){
+    const splitChar = str.split("")
+    const counts = {};
+    let maxCount = 0;
+    let mostFrequent = "";
+    for (let i = 0; i <= splitChar.length; i++){
+        const char = splitChar[i]
+        if (counts[char] !== undefined){
+            counts[char] += 1;
+        } else {
+            counts[char] = 1;
+        }
+        if (counts[char] > maxCount){
+            // CONFUSED, I DON'T NOT HOW TO DISPLAY THE "a"... 
+            maxCount = counts[char];
+            mostFrequent = counts; // "2" It's a number...
+        }
+    }
+    return maxCount;
+}
+
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
+
+
+// return counts;
+
+    // for (const count in counts){
+    //     if (counts[char] > maxCount){
+    //         maxCount = count[char];
+    //         mostFrequent = count;
+    //     }
+    // }
