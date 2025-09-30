25e0c765dfcc2982cccb94738453d5306642ab77
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-29 19:47:57 -0700

    exercise 03 is completed

--- ex03.js
+++ ex03.js
@@ -2,4 +2,36 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+// for loop
+// loop i = 1, i <= array.length +1
+// check -> if the arr inclueds the i or not
+// if not includes (!) return i
+
+const findMissingNumber = function (arr) {
+    for (let i = 1; i <= arr.length + 1; i++){
+        if (!arr.includes(i)){
+            return i;
+        }
+    }
+}
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
+
+// const lagestNum = arr.length + 1;
+//     return lagestNum;
+
+// const lagestNum = arr.length + 1;
+//     arr.filter((item) => {
+//         if (item)
+//     })
+
+// const findLagestNum = arr.find((item, cur) => {
+    //         if (item >= cur) {
+    //             findLagestNum = item;
+    //             return findLagestNum;
+    //         }
+    //     })
+
+    // return arr.find((item) => {
+    //     if ()
+    // })
