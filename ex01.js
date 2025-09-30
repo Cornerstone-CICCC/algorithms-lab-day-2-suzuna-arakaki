098c6d114dee39240ab5530df8a07ea564b70d9b
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-29 17:57:55 -0700

    exercise 01 is completed

--- ex01.js
+++ ex01.js
@@ -2,4 +2,13 @@
 // with the first letter of each word capitalized.
 // Example: capitalizeWords("hello world") should return "Hello World".
 
+const capitalizeWords = function (input){
+    const words = input.split(" ");
+    const capitalized = words.map(word => {
+        let slicedWords = word[0].toUpperCase() + word.slice(1);
+        return slicedWords;
+    })
+    return slicedWords = capitalized.join(" ");
+}
+
 console.log(capitalizeWords("hello world")); // Expected output: "Hello World"
