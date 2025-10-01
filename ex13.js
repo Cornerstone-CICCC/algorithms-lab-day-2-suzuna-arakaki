c9d031be0b6fcc0ebd216c6c057076dc544faacc
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-30 21:38:54 -0700

    exercise 13 is conpleted

--- ex13.js
+++ ex13.js
@@ -2,4 +2,16 @@
 // and returns the sum of all numbers from 1 to `n`.
 // Example: sumRange(5) should return 15 (1 + 2 + 3 + 4 + 5).
 
+const sumRange = function (n){
+    const array = [];
+    for (let i = 1; i <= n; i++){
+        array.push(i)
+    }
+
+    const sumNum = array.reduce((sum, num) => {
+            return sum + num;
+    }, 0);
+    return sumNum;
+}
+
 console.log(sumRange(5)); // Expected output: 15
