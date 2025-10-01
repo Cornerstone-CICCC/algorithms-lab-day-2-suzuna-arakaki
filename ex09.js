8e0ed618f1c702c677c1efb999835460a4c8bc6c
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-30 22:41:31 -0700

    exercise 09 is conpleted

--- ex09.js
+++ ex09.js
@@ -3,4 +3,20 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
+const sortByProperty = function (item) {
+    // const person = item; // I didn't need it...
+    function comparatorFunction(a, b) {
+        if(a.age > b.age) return 1;
+        if(a.age < b.age) return -1;
+        return 0;
+    }
+    return item.sort(comparatorFunction);
+}
+
 console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
+
+// const ageChecker = person.filter((key) => {
+//         const personAge = key.age
+//         return personAge;
+//     })
+//     return ageChecker.sort(comparatorFunction);
