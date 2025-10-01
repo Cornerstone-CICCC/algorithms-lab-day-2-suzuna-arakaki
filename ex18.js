98da87d0a92dd36f2612730edcdff72c5a5a22b8
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-30 20:12:35 -0700

    exercise 18 is completed

--- ex18.js
+++ ex18.js
@@ -3,15 +3,40 @@
 // 2. Reformat fullName to "LastName, FirstName(s)"
 // 3. Sort alphabetically by last name
 
-const people = [
+//
+const studentsArray = function (array){
+  const studentFilter = array.filter((item) => {
+    if(item.isStudent === true){
+      return item;
+    }
+  })
+  const studentMap = studentFilter.map((person) => {
+    const splitFullName = person.fullName.split(" ");
+    const lastName = splitFullName.pop();
+    const firstNames = splitFullName.join(" ");
+    const personsFullName = `${lastName}, ${firstNames}`
+    return personsFullName;
+  })
+  return studentMap.sort();
+}
+
+console.log(studentsArray([
   { fullName: "John Smith", isStudent: true },
   { fullName: "Jane B. Doe", isStudent: false },
   { fullName: "Charlie Miguel Chaplin", isStudent: true },
   { fullName: "Zara Ali", isStudent: true }
-]
+]))
 
-console.log(
-  people
-)
+// const people = [
+//   { fullName: "John Smith", isStudent: true },
+//   { fullName: "Jane B. Doe", isStudent: false },
+//   { fullName: "Charlie Miguel Chaplin", isStudent: true },
+//   { fullName: "Zara Ali", isStudent: true }
+// ]
+
+// console.log(
+//   people
+// )
+
 // Expected output: (array of strings)
 // ["Ali, Zara", "Chaplin, Charlie Miguel", "Smith, John"]
