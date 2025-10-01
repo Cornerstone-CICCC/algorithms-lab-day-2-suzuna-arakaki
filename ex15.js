f63974ad41b2e387993680354f3e3c102e01def4
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-30 21:02:27 -0700

    exercise 15 is conpleted

--- ex15.js
+++ ex15.js
@@ -2,4 +2,24 @@
 // and returns the maximum number in the array.
 // Example: findMax([3, 1, 4, 1, 5, 9]) should return 9.
 
+const findMax = function (array){
+    const findMaxNum = array.reduce((max, item) => {
+        if (item > max){
+            return item
+        } else {
+            return max
+        }
+    }, 0)
+    return findMaxNum;
+}
+
 console.log(findMax([3, 1, 4, 1, 5, 9])); // Expected output: 9
+
+
+// const findMaxNum = array.filter((item) => {
+//         let maxNum = 0;
+//         if (item > maxNum){
+//             maxNum = item;
+//             return maxNum;
+//         }
+//     })
